37a4e33c77558a6c1b0eadd52a9e96e58ec7b479
zafar <zafarnaqvi6@gmail.com>
2019-11-15 01:20:10 +0500

    Assignment 2

---
+++ Assignment 2/Q5.py
@@ -0,0 +1,10 @@
+
+5. Write a Python program to get the largest number from a numeric list.
+
+
+lst = []
+num = int(input('How many numbers: '))
+for n in range(num):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+print("Maximum element in the list is :", max(lst), "\nMinimum element in the list is :", min(lst))
