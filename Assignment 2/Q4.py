351194dd339e055cb9ccf8a8443ec8eeedd3fb60
zafar <zafarnaqvi6@gmail.com>
2019-11-15 01:36:39 +0500

    Assignment 2

---
+++ Assignment 2/Q4.py
@@ -0,0 +1,9 @@
+
+4. Write a Python program to sum all the numeric items in a list?
+
+lst = []
+num = int(input('How many numbers: '))
+for n in range(num):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+print("Sum of elements in given list is :", sum(lst))
